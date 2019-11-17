e25d9a39bf4531acbbb7b5128ddccebf6f6fdcb0
Brian Nicholls <nichol88@gmail.com>
2019-11-17 00:49:34 -0500

    added super basic CLI functionality

--- lib/main.rb
+++ lib/main.rb
@@ -2,18 +2,34 @@ require 'httparty'
 require 'nokogiri'
 require 'pry'
 
-page = HTTParty.get(‘https://www.reddit.com/r/popular/’)
+class Main
 
-# puts page.code
+def initialize # initialize, ask user for subreddit name, display info
+  @page
+  set_subreddit
+  display_authors_current_page
+end
 
-authors = page.body.split("author\":\"")
+def get_authors_from_page # returns array of authors on @page
+  authors = @page.body.split("author\":\"")
+  authors = authors.map{ |a|
+    a[0..a.index(",") - 2]
+  }
+  authors.shift
+  authors
+end
 
-# authors[10].index("\\")
+def set_subreddit #### DOESN'T ALWAYS GET PAGE - INVESTIGATE
+  puts "Enter subreddit name:"
+  subreddit = gets.strip
+  @page = HTTParty.get("http://reddit.com/r/#{subreddit}")
+end
 
-# authors[10][0..authors[10].index(",")]
+def display_authors_current_page # prints all authors on @page
+  authors = get_authors_from_page
+  authors.each{|author|
+    p "#{author}"
+  }
+end
 
-authors = authors.map{ |a|
-  a[0..a.index(",") - 1]
-}
-
-authors.shift
+end
